1ba65c5694e15ee2d76840809a5e52326ef4458c
junjunjjjun <junoo.par@gmail.com>
2025-06-09 12:41:13 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,7 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import playformInline from "@playform/inline";
-
-import mdx from "@astrojs/mdx";
-
-import alpinejs from "@astrojs/alpinejs";
-
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-	site: "https://pawstronaut.netlify.app",
-	base: "/",
-	integrations: [
-		tailwind(),
-		alpinejs(),
-		mdx(),
-		(await import("@playform/inline")).default({
-			Critters: true,
-		}),
-	],
-	output: "static",
-	devToolbar: {
-		enabled: false,
-	},
-	experimental: {
-		svg: true,
-	},
+  integrations: [tailwind()],
+  output: 'static', // optional, but good to include for clarity
 });
